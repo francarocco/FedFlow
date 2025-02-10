e8c9130710b1441c0d5b049017e52b9b374b02cf
Franca Rocco di Torrepadula <franca.rocco@gmail.com>
2025-02-10 11:02:59 +0100

    perfedavg baseline

--- code/utilities/networks.py
+++ code/utilities/networks.py
@@ -11,6 +11,15 @@ def lstm_definition(input_shape, output_shape):
     model.compile(loss='mean_absolute_error', optimizer=tf.keras.optimizers.Adam(),metrics=['mean_absolute_error','mean_absolute_percentage_error','mean_squared_error'])
     return model
 
+# LSTM network definition
+def lstm_definition_no_compiling(input_shape, output_shape):
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.LSTM(128, input_shape=input_shape,return_sequences=True)) # -> 1 input layer, 1  intermediate layer
+    model.add(tf.keras.layers.LSTM(128,return_sequences=True)) # -> another hidden 
+    model.add(tf.keras.layers.LSTM(32)) # -> another hidden 
+    model.add(tf.keras.layers.Dense(output_shape))  # -> output layer
+    return model
+
 # CNN network defintion
 def cnn_definition(input_shape, output_shape):
     model = tf.keras.models.Sequential()
